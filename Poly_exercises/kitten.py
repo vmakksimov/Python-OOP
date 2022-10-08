e596e1a5a54823e67a0630243e387a0dff91a377
Viktor <vmakksimov@gmail.com>
2022-10-08 03:00:26 +0300

    https://github.com/vmakksimov/Python-OOP.git

---
+++ Poly_exercises/kitten.py
@@ -0,0 +1,13 @@
+from Polymorhisym_abstraction.Poly_exercises.cat import Cat
+
+
+class Kitten(Cat):
+    def __init__(self, name, age):
+        super().__init__(name, age, gender='Female')
+
+    def make_sound(self):
+        return 'Meow'
+
+
+    def __repr__(self):
+        return f'This is {self.name}. {self.name} is a {self.age} year old {self.gender} {__class__.__name__}'
